a6cac4527e14960214ff26a42436dcde2a4eced7
Humoyun Mahmudov <humoyunmirzomahmudov99@gmail.com>
2023-01-19 10:30:24 +0500

    install (axios, json-server), create (db.json, EventService)

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -8,6 +8,7 @@
 <script>
 // @ is an alias to /src
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService";
 
 export default {
   name: "EventList",
@@ -16,42 +17,17 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 5928101,
-          category: "animal walfare",
-          title: "Cat Adoption Day",
-          description: "Find your new feline friend at this event",
-          location: "Meow Town",
-          date: "January 28, 2022",
-          time: "12:00",
-          petsAllowed: true,
-          organizer: "Kat Laydee",
-        },
-        {
-          id: 5928102,
-          category: "animal walfare",
-          title: "Cat Adoption Day",
-          description: "Find your new feline friend at this event",
-          location: "Meow Town",
-          date: "January 28, 2022",
-          time: "12:00",
-          petsAllowed: true,
-          organizer: "Kat Laydee",
-        },
-        {
-          id: 5928103,
-          category: "animal walfare",
-          title: "Cat Adoption Day",
-          description: "Find your new feline friend at this event",
-          location: "Meow Town",
-          date: "January 28, 2022",
-          time: "12:00",
-          petsAllowed: true,
-          organizer: "Kat Laydee",
-        },
-      ],
+      events: null,
     };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        this.events = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
