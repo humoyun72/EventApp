af127c92ee004138137ed1b6e9aeeaf5117f5ced
Humoyun Mahmudov <humoyunmirzomahmudov99@gmail.com>
2023-01-19 11:52:31 +0500

    create (EventDetails) page

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import AboutView from "../views/AboutView.vue";
+import EventDetails from "@/views/EventDetails";
 
 const routes = [
   {
     path: "/",
     name: "EventList",
     component: EventList,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails,
   },
   {
     path: "/about",
